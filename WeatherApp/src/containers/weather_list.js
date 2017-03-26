2e4f23e49ff1b2720bcb92fbfcb9df013f975c0e
Shabin Hashim <shabin_hashim01@infosys.com>
2017-03-26 20:05:39 +0530

    changes

--- WeatherApp/src/containers/weather_list.js
+++ WeatherApp/src/containers/weather_list.js
@@ -1,6 +1,7 @@
 import React , {Component} from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/googe_map';
 
 
 class WeatherList extends Component {
@@ -8,13 +9,25 @@ class WeatherList extends Component {
   renderWeather(cityData){
     //based on response from open weather api
     //es6 shortcut instead of pushng the value to array
-    const temps = cityData.list.map(weather => weather.main.temp);
-    console.log(temps);
+    const temps = cityData.list.map(weather => (weather.main.temp-273) );
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const hummidities = cityData.list.map(weather => weather.main.humidity);
+    // const lon = cityData.city.coord.lon;
+    // const lat = cityData.city.coord.lat;
+    const { lon , lat } = cityData.city.coord;//shortcut to above..es6
+
+
     return(
       <tr key={cityData.city.name}>
-        <td>{cityData.city.name}</td>
+        <td><GoogleMap lon={lon} lat={lat}/></td>
         <td>
-          <Chart color="orange" data={temps} />
+          <Chart color="orange" data={temps} units="C"/>
+        </td>
+        <td>
+          <Chart color="green" data={pressures} units="hPa"/>
+        </td>
+        <td>
+          <Chart color="black" data={hummidities} units="%"/>
         </td>
       </tr>
     )
@@ -26,9 +39,9 @@ render() {
       <thead>
         <tr>
           <th>City</th>
-          <th>Temperature</th>
-          <th>Pressure</th>
-          <th>Humidity</th>
+          <th>Temperature (C)</th>
+          <th>Pressure(hPa)</th>
+          <th>Humidity(%)</th>
         </tr>
       </thead>
       <tbody>
